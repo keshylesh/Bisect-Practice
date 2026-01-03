ad10199ce19e2a913c46e936a3522f8bd4068ae0
keshylesh <keshavshylesh@gmail.com>
2026-01-03 03:41:10 -0800

    rats

--- calc.cc
+++ calc.cc
@@ -45,3 +45,9 @@ int main() {
 
     return 0;
 }
+
+/* Crazy? I was crazy once. They locked me in a room. A rubber room. A rubber room with rats. And rats make me crazy. 
+ * Crazy? I was crazy once. They locked me in a room. A rubber room. A rubber room with rats. And rats make me crazy.
+ * Crazy? I was crazy once. They locked me in a room. A rubber room. A rubber room with rats. And rats make me crazy.
+ * Crazy? I was crazy once. They locked me in a room. A rubber room. A rubber room with rats. And rats make me crazy.
+ */
