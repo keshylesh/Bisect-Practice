d05d2cf655e7af3b8c321b1a2f675f6218226e85
keshylesh <keshavshylesh@gmail.com>
2026-01-03 03:01:42 -0800

    Add multiplication

--- calc.cc
+++ calc.cc
@@ -1,6 +1,12 @@
 #include <cstdio>
 
-/* Basic program to add two numbers! */
+/* Basic program to add two numbers! (Now with multiplication! o_o)*/
+
+enum choice {
+    no_choice,
+    add,
+    mult
+};
 
 int main() {
     int a,b;
@@ -8,7 +14,20 @@ int main() {
     scanf("%d", &a);
     scanf("%d", &b);
 
-    printf("%d\n", a+b);
+    enum choice c = no_choice;
+    while (c == no_choice) {
+        printf("1: add\n2: multiply\n");
+        scanf("%d", (int *) &c);
+
+        c = (c == add || c == mult) ? c : no_choice;
+    }
+
+    if (c == add) {
+        printf("%d\n", a+b);
+    }
+    else {
+        printf("%d\n", a*b);
+    }
 
     return 0;
 }
